a908d589cf122eb5ee6b7bbc9f047f127e1a2c7a
Mohsen <mcmfarahanchi@gmail.com>
2016-04-25 17:18:49 +0500

    section1

--- src/app.py
+++ src/app.py
@@ -1,6 +1,7 @@
-
 import requests
 
-requests = requests.get("http://www.google.com/")
+request = requests.get("http://www.digikala.com/Product/DKP-21146/")
+
+# <span id="frmLblPayablePriceAmount" class="price" itemprop="price" content="13990000.00">1,399,000</span>
 
 print(request.content)
